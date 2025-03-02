869275560f207f254f9ddfa772632aac486dc3ca
DavidFerrs <fernandes.-.david@hotmail.com>
2025-03-02 15:08:01 -0300

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by David Ferreira on 02/03/25.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
