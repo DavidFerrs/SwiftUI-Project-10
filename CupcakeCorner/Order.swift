58bdf1e021812e46d55a6733d9ad18834502aded
DavidFerrs <fernandes.-.david@hotmail.com>
2025-03-03 18:13:43 -0300

    store user address localy with UserDefaults

--- CupcakeCorner/Order.swift
+++ CupcakeCorner/Order.swift
@@ -15,12 +15,8 @@ class Order: Codable {
         case _specialRequestEnable = "specialRequestEnable"
         case _addSprinkles = "addSprinkles"
         case _extraFrosting = "extraFrosting"
-        case _name = "name"
-        case _streetName = "streetName"
-        case _city = "city"
-        case _zip = "zip"
-
     }
+    
     static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
     
     var type = 0
@@ -34,21 +30,16 @@ class Order: Codable {
             }
         }
     }
+    
     var extraFrosting = true
     var addSprinkles = true
     
-    
-    var name = ""
-    var streetName = ""
-    var city = ""
-    var zip = ""
-    
-    var hasValidAddress: Bool {
-        if name.isEmpty || streetName.isEmpty || city.isEmpty || zip.isEmpty {
-            return false
+    var address = Address() {
+        didSet {
+            if let encoded = try? JSONEncoder().encode(address) {
+                UserDefaults.standard.set(encoded, forKey: "Address")
+            }
         }
-        
-        return true
     }
     
     var cost: Decimal {
@@ -65,5 +56,40 @@ class Order: Codable {
         }
         
         return cost
+    }
+    
+}
+
+@Observable
+class Address: Codable  {
+    enum CodingKeys: String, CodingKey {
+        case _name = "name"
+        case _streetName = "streetName"
+        case _city = "city"
+        case _zip = "zip"
+    }
+    
+    var name = ""
+    var streetName = ""
+    var city = ""
+    var zip = ""
+    
+    var hasValidAddress: Bool {
+        if name.isEmpty || streetName.isEmpty || city.isEmpty || zip.isEmpty {
+            return false
+        }
+        
+        return true
+    }
+    
+    init() {
+        if let savedAddress = UserDefaults.standard.data(forKey: "Address") {
+            if let decoded = try? JSONDecoder().decode(Address.self, from: savedAddress) {
+                name.self = decoded.name
+                streetName.self = decoded.streetName
+                city.self = decoded.city
+                zip.self = decoded.zip
+            }
+        }
     }
 }
